0e60a8188bcdfa2bcc3fbb2199d857ed2138561e
gr-terada <wataru.terada@gmo-research.jp>
2014-01-11 21:30:16 +0900

    the first commit

---
+++ Test/Case/Test/TestLib/ProtectBreakMagicTest.php
@@ -0,0 +1,263 @@
+<?php
+App::uses('ProtectBreakMagic', 'ProtectBreakMagic.Test/TestLib');
+
+/**
+ * Class ProtectBreakMagicTest
+ *
+ * todo: trait
+ *
+ *
+ * @property object $Mock  //You can hide warnings about protected accesses on PHPStorm when you define it as 'object' type.
+ * @property object $MockExtend
+ */
+class ProtectBreakMagicTest extends CakeTestCase {
+
+    public function provideForMocks() {
+        return array(
+            array(new ProtectBreakMagic(new MockProtectBreakMagic()), "通常のパターン"),
+            array(new ProtectBreakMagic(new MockProtectBreakMagic_extend()), "すべて継承元を参照するパターン"),
+            array(new ProtectBreakMagic(new MockProtectBreakMagic_magic()), "magicメソッド持ってるパターン"),
+        );
+    }
+
+    /**
+     * procated なプロパティにアクセスできる
+     *
+     * @dataProvider provideForMocks
+     * @param object $mock
+     * @param string $message
+     */
+    public function test_can_access_protected_property($mock, $message) {
+        $this->assertTrue(property_exists($mock->object, "protected_property"), $message); //createしてないことを確定させるため、念のために存在チェック
+        $mock->protected_property = "protected";
+        $this->assertEquals("protected", $mock->protected_property, $message);
+    }
+
+    /**
+     * procated なメソッドにアクセスできる
+     *
+     * @dataProvider provideForMocks
+     * @param object $mock
+     * @param string $message
+     */
+    public function test_can_call_protected_method($mock, $message) {
+        $this->assertEquals("+#-protected", $mock->protectedMethod("protected"), $message);
+    }
+
+    /**
+     * public なプロパティにアクセスできる
+     *
+     * @dataProvider provideForMocks
+     * @param object $mock
+     * @param string $message
+     */
+    public function test_can_access_public_property($mock, $message) {
+        $this->assertTrue(property_exists($mock->object, "public_property"), $message); //createしてないことを確定させるため、念のために存在チェック
+        $mock->public_property = "public";
+        $this->assertEquals("public", $mock->public_property, $message);
+    }
+
+    /**
+     * public なメソッドにアクセスできる
+     *
+     * @dataProvider provideForMocks
+     * @param object $mock
+     * @param string $message
+     */
+    public function test_can_call_public_method($mock, $message) {
+        $this->assertEquals("+#-public", $mock->publicMethod("public"), $message);
+    }
+
+    /**
+     * 存在しないプロパティを新規に作成できる
+     *
+     * @dataProvider provideForMocks
+     * @param object $mock
+     * @param string $message
+     */
+    public function test_can_create_new_property($mock, $message) {
+        $this->assertFalse(property_exists($mock->object, "new_property"), $message); //createすることを確定させるため、念のために存在チェック
+        $mock->protected_property = "protected";
+        $mock->public_property = "public";
+        $mock->new_property = "new";
+        $this->assertEquals("protected", $mock->protected_property, $message); //no effect
+        $this->assertEquals("public", $mock->public_property, $message); //no effect
+        $this->assertEquals("new", $mock->new_property, $message);
+    }
+
+    /**
+     * private なプロパティにセットできない
+     *
+     * @expectedException LogicException
+     * @expectedExceptionMessage should not set-access
+     */
+    public function test_cannot_set_private_property() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic());
+        $mock->private_property = "private";
+    }
+
+    /**
+     * private なプロパティをゲットできない
+     *
+     * @expectedException LogicException
+     * @expectedExceptionMessage should not get-access
+     */
+    public function test_cannot_get_private_property() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic());
+        $this->assertEquals(null, $mock->private_property);
+    }
+
+    /**
+     * private なメソッドにアクセスできない
+     *
+     * @expectedException LogicException
+     * @expectedExceptionMessage should not call
+     */
+    public function test_cannot_call_private_method() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic());
+        $mock->privateMethod("private");
+    }
+
+    /**
+     * 親クラスで private だったが子クラスで protected になっていてもアクセスできる
+     */
+    public function test_can_access_even_if_changing_to_protected() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic_toProtected());
+
+        $this->assertTrue(property_exists($mock->object, "private_property")); //createしてないことを確定させるため、念のために存在チェック
+        $mock->private_property = "to_protected";
+        $this->assertEquals("to_protected", $mock->private_property);
+
+        $this->assertEquals("++##--to_protected", $mock->privateMethod("to_protected"));
+    }
+
+    /**
+     * 子クラスで override されているプロパティでも親クラスのメソッドから参照される
+     */
+    public function test_can_be_accessed_even_if_overridden_property() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic_overrideProperty());
+        $mock->protected_property = "overridden_property";
+        $this->assertEquals("overridden_property", $mock->getProtectedProperty());
+    }
+
+    /**
+     * magicメソッドを実装している場合でも正常に呼ばれる
+     */
+    public function test_magic_methods_is_called__set() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic_magic());
+        $mock->public_property = "public";
+        $mock->protected_property = "protected";
+
+        $this->assertEquals(0, $mock->counts['__set']);
+        $mock->new_property = "new";
+        $this->assertEquals(1, $mock->counts['__set']);
+    }
+
+    public function test_magic_methods_is_called__get() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic_magic());
+
+        $this->assertSame('', $mock->public_property);
+        $this->assertSame('', $mock->protected_property);
+
+        $this->assertEquals(0, $mock->counts['__get']);
+        $this->assertSame(null, $mock->new_property);
+        $this->assertEquals(1, $mock->counts['__get']);
+    }
+
+    public function test_magic_methods_is_called__call() {
+        /** @var object $mock */
+        $mock = new ProtectBreakMagic(new MockProtectBreakMagic_magic());
+        $this->assertEquals("+#-public", $mock->publicMethod("public"));
+        $this->assertEquals("+#-protected", $mock->protectedMethod("protected"));
+
+        $this->assertEquals(0, $mock->counts['__call']);
+        $this->assertEquals("new", $mock->newMethod("new"));
+        $this->assertEquals(1, $mock->counts['__call']);
+    }
+}
+
+
+class MockProtectBreakMagic {
+    public $public_property = "";
+    protected $protected_property = "";
+    private $private_property = "";
+
+    public function publicMethod($arg) {
+        return $this->privateMethod($arg);
+    }
+
+    protected function protectedMethod($arg) {
+        return $this->privateMethod($arg);
+    }
+
+    private function privateMethod($arg) {
+        $this->public_property = "+";
+        $this->protected_property = "#";
+        $this->private_property = "-";
+        return $this->public_property . $this->protected_property . $this->private_property . $arg;
+    }
+
+    public function getProtectedProperty() {
+        return $this->protected_property;
+    }
+}
+
+/**
+ * すべてを継承
+ */
+class MockProtectBreakMagic_extend extends MockProtectBreakMagic {
+}
+
+/**
+ * 同じ名前で protected になっている
+ */
+class MockProtectBreakMagic_toProtected extends MockProtectBreakMagic {
+    protected $private_property = "";
+
+    protected function privateMethod($arg) {
+        $this->public_property = "++";
+        $this->protected_property = "##";
+        $this->private_property = "--";
+        return $this->public_property . $this->protected_property . $this->private_property . $arg;
+    }
+}
+
+/**
+ * プロパティをoverride
+ */
+class MockProtectBreakMagic_overrideProperty extends MockProtectBreakMagic {
+    protected $protected_property = "";
+}
+
+/**
+ * すべてを継承
+ */
+class MockProtectBreakMagic_magic extends MockProtectBreakMagic {
+    public $counts = array(
+        '__call' => 0,
+        '__get'  => 0,
+        '__set'  => 0,
+    );
+
+    public function __call($method, $arguments) {
+        $this->counts['__call']++;
+        return $arguments[0];
+    }
+
+    public function __get($name) {
+        $this->counts['__get']++;
+        return null;
+    }
+
+    public function __set($name, $value) {
+        $this->counts['__set']++;
+        $this->$name = $value;
+    }
+}
---
+++ Test/TestLib/ProtectBreakMagic.php
@@ -0,0 +1,63 @@
+<?
+/**
+ * protected なメソッドにアクセスできるようにするラッパーオブジェクト。
+ */
+class ProtectBreakMagic {
+
+    public $object;
+    private $ref;
+
+    /**
+     * コンストラクタ。
+     * @param object $object
+     */
+    public function __construct($object) {
+        $this->object = $object;
+        $this->ref = new ReflectionClass(get_class($object));
+    }
+
+    public function __call($method, $arguments) {
+        if (! method_exists($this->object, $method) && method_exists($this->object, '__call')) {
+            return call_user_func_array(array($this->object, $method), $arguments);
+        }
+        $methodObj = $this->ref->getMethod($method);
+        if ($methodObj->isPrivate()) {
+            throw new LogicException($method . '() is a private method, so you should not call it from outside even if testing.');
+        }
+        $methodObj->setAccessible(true);
+        return $methodObj->invokeArgs($this->object, $arguments);
+    }
+
+    public function __get($name) {
+        if (property_exists($this->object, $name)) {
+            try {
+                $propObj = $this->ref->getProperty($name);
+            } catch (ReflectionException $e) {
+                return $this->object->$name; //定義にはないが、動的に追加されたプロパティ
+            }
+            if ($propObj->isPrivate()) {
+                throw new LogicException($name . ' is a private property, so you should not get-access it from outside even if testing.');
+            }
+            $propObj->setAccessible(true);
+            return $propObj->getValue($this->object);
+        } else {
+            return $this->object->$name; //__get が呼ばれるように
+        }
+    }
+
+    public function __set($name, $value) {
+        if ($this->ref->hasProperty($name)) {
+            //set the value after the accessibility is changed to the public if exists
+            $propObj = $this->ref->getProperty($name);
+            if ($propObj->isPrivate()) {
+                throw new LogicException($name . ' is a private property, so you should not set-access it from outside even if testing.');
+            }
+            $propObj->setAccessible(true);
+            $propObj->setValue($this->object, $value);
+        } else {
+            //set the value newly if not exists
+            $this->object->$name = $value;
+        }
+    }
+}
+
